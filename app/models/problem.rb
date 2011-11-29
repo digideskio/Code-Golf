f826445d9b3fd8d4f5eb90ccccb30363b288e6ae
= <=>
2011-11-28 20:21:20 -0500

    can mass assign sample_output

--- app/models/problem.rb
+++ app/models/problem.rb
@@ -5,5 +5,5 @@ class Problem < ActiveRecord::Base
   validates :description, :presence => true
   validates :par_score, :presence => true, :numericality => true
   
-  attr_accessible :title, :description, :par_score, :language, :current_problem
+  attr_accessible :title, :description, :par_score, :language, :current_problem, :sample_output
 end
